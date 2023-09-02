00b627f4a35c3170ac96abbec1de3afe4a88c41e
Charles Xie <charxie@gmail.com>
2023-09-02 14:20:45 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './images/ifi-logo.png';
+import aimsLogo from './images/aims-logo-300.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <img src={aimsLogo} alt="logo" />
         <p>
           AIMS: Artificial Intelligence for Molecular Sciences
         </p>
