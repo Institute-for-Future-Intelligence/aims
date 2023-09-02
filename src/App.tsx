3f02fff4ec0e72876045112e701f4f3097433f74
Charles Xie <charxie@gmail.com>
2023-09-02 11:10:55 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/ifi-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          AIMS: Artificial Intelligence for Molecular Sciences
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Under construction...
       </header>
     </div>
   );
