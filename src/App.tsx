9cdb55192ee1c2c57f83689bd3cf682dbdae6a05
xiaotong <xd59@njit.edu>
2023-12-11 17:06:49 -0500

    format check

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,51 @@
  * @Copyright 2023-2024. Institute for Future Intelligence, Inc.
  */
 
-import React, {useMemo} from 'react';
+import React, { useMemo } from 'react';
 import './App.css';
 import i18n from './i18n/i18n';
-import {useStore} from "./stores/common";
+import { useStore } from './stores/common';
 import * as Selector from './stores/selector';
-import {visitHomepage} from "./helpers";
-import MainMenu from "./mainMenu";
+import { visitHomepage } from './helpers';
+import MainMenu from './mainMenu';
 
-const App= () => {
+const App = () => {
+  const language = useStore(Selector.language);
 
-    const language = useStore(Selector.language);
+  const lang = useMemo(() => {
+    return { lng: language };
+  }, [language]);
 
-    const lang = useMemo(() => {
-        return {lng: language};
-    }, [language]);
+  console.log('check');
 
-    return (
-        <div className="App">
-            <div
-                style={{
-                    backgroundColor: 'lightblue',
-                    height: '72px',
-                    paddingTop: '10px',
-                    textAlign: 'start',
-                    userSelect: 'none',
-                    fontSize: '30px',
-                }}
-            >
+  return (
+    <div className="App">
+      <div
+        style={{
+          backgroundColor: 'lightblue',
+          height: '72px',
+          paddingTop: '10px',
+          textAlign: 'start',
+          userSelect: 'none',
+          fontSize: '30px',
+        }}
+      >
         <span
-            style={{
-                marginLeft: '120px',
-                verticalAlign: 'middle',
-                cursor: 'pointer',
-                userSelect: 'none',
-            }}
-            title={i18n.t('tooltip.visitAIMSHomePage', lang)}
-            onClick={visitHomepage}
+          style={{
+            marginLeft: '120px',
+            verticalAlign: 'middle',
+            cursor: 'pointer',
+            userSelect: 'none',
+          }}
+          title={i18n.t('tooltip.visitAIMSHomePage', lang)}
+          onClick={visitHomepage}
         >
           {`${i18n.t('name.AIMS', lang)}`}
         </span>
-            </div>
-            <MainMenu
-                viewOnly={false}
-                canvas={null}
-            />
-        </div>
-    );
-}
+      </div>
+      <MainMenu viewOnly={false} canvas={null} />
+    </div>
+  );
+};
 
 export default React.memo(App);
