2955430edffe60d5f269199b8192be2d84144f08
Charles Xie <charxie@gmail.com>
2023-12-15 10:37:40 -0500

    rewrote pdb loader

--- src/js/MyPDBLoader.js
+++ src/js/MyPDBLoader.js
@@ -1,4 +1,10 @@
-import { BufferGeometry, FileLoader, Float32BufferAttribute, Loader, Color } from 'three';
+/*
+ * Revised from three.js library
+ *
+ * @Copyright 2024. Institute for Future Intelligence, Inc.
+ */
+
+import { BufferGeometry, FileLoader, Float32BufferAttribute, Loader } from 'three';
 
 class MyPDBLoader extends Loader {
   constructor(manager) {
@@ -69,6 +75,7 @@ class MyPDBLoader extends Loader {
       const build = {
         geometryAtoms: new BufferGeometry(),
         geometryBonds: new BufferGeometry(),
+        elementsBonds: [],
         json: {
           atoms: atoms,
         },
@@ -76,236 +83,57 @@ class MyPDBLoader extends Loader {
 
       const geometryAtoms = build.geometryAtoms;
       const geometryBonds = build.geometryBonds;
-
       const verticesAtoms = [];
-      const colorsAtoms = [];
       const verticesBonds = [];
-      const colorsBonds = [];
 
       // atoms
-
-      const c = new Color();
-
       for (let i = 0, l = atoms.length; i < l; i++) {
         const atom = atoms[i];
-
-        const x = atom[0];
-        const y = atom[1];
-        const z = atom[2];
-
-        verticesAtoms.push(x, y, z);
-
-        const r = atom[3][0] / 255;
-        const g = atom[3][1] / 255;
-        const b = atom[3][2] / 255;
-
-        c.set(r, g, b).convertSRGBToLinear();
-
-        colorsAtoms.push(c.r, c.g, c.b);
+        verticesAtoms.push(atom[0], atom[1], atom[2]); // x, y, z
       }
 
       // bonds
-
       for (let i = 0, l = _bonds.length; i < l; i++) {
         const bond = _bonds[i];
-
         const start = bond[0];
         const end = bond[1];
-
         const startAtom = _atomMap[start];
         const endAtom = _atomMap[end];
-
-        let x = startAtom[0];
-        let y = startAtom[1];
-        let z = startAtom[2];
-
-        verticesBonds.push(x, y, z);
-
-        let r = startAtom[3][0] / 255;
-        let g = startAtom[3][1] / 255;
-        let b = startAtom[3][2] / 255;
-
-        c.set(r, g, b).convertSRGBToLinear();
-
-        colorsBonds.push(c.r, c.g, c.b);
-
-        x = endAtom[0];
-        y = endAtom[1];
-        z = endAtom[2];
-
-        verticesBonds.push(x, y, z);
-
-        r = endAtom[3][0] / 255;
-        g = endAtom[3][1] / 255;
-        b = endAtom[3][2] / 255;
-
-        c.set(r, g, b).convertSRGBToLinear();
-
-        colorsBonds.push(c.r, c.g, c.b);
+        verticesBonds.push(startAtom[0], startAtom[1], startAtom[2]); // x, y, z for start atom
+        build.elementsBonds.push(startAtom[3]); // element for start atom
+        verticesBonds.push(endAtom[0], endAtom[1], endAtom[2]); // x, y, z for end atom
+        build.elementsBonds.push(endAtom[3]); // element for end atom
       }
 
       // build geometry
-
       geometryAtoms.setAttribute('position', new Float32BufferAttribute(verticesAtoms, 3));
-      geometryAtoms.setAttribute('color', new Float32BufferAttribute(colorsAtoms, 3));
-
       geometryBonds.setAttribute('position', new Float32BufferAttribute(verticesBonds, 3));
-      geometryBonds.setAttribute('color', new Float32BufferAttribute(colorsBonds, 3));
 
       return build;
     }
 
-    const CPK = {
-      h: [255, 255, 255],
-      he: [217, 255, 255],
-      li: [204, 128, 255],
-      be: [194, 255, 0],
-      b: [255, 181, 181],
-      c: [144, 144, 144],
-      n: [48, 80, 248],
-      o: [255, 13, 13],
-      f: [144, 224, 80],
-      ne: [179, 227, 245],
-      na: [171, 92, 242],
-      mg: [138, 255, 0],
-      al: [191, 166, 166],
-      si: [240, 200, 160],
-      p: [255, 128, 0],
-      s: [255, 255, 48],
-      cl: [31, 240, 31],
-      ar: [128, 209, 227],
-      k: [143, 64, 212],
-      ca: [61, 255, 0],
-      sc: [230, 230, 230],
-      ti: [191, 194, 199],
-      v: [166, 166, 171],
-      cr: [138, 153, 199],
-      mn: [156, 122, 199],
-      fe: [224, 102, 51],
-      co: [240, 144, 160],
-      ni: [80, 208, 80],
-      cu: [200, 128, 51],
-      zn: [125, 128, 176],
-      ga: [194, 143, 143],
-      ge: [102, 143, 143],
-      as: [189, 128, 227],
-      se: [255, 161, 0],
-      br: [166, 41, 41],
-      kr: [92, 184, 209],
-      rb: [112, 46, 176],
-      sr: [0, 255, 0],
-      y: [148, 255, 255],
-      zr: [148, 224, 224],
-      nb: [115, 194, 201],
-      mo: [84, 181, 181],
-      tc: [59, 158, 158],
-      ru: [36, 143, 143],
-      rh: [10, 125, 140],
-      pd: [0, 105, 133],
-      ag: [192, 192, 192],
-      cd: [255, 217, 143],
-      in: [166, 117, 115],
-      sn: [102, 128, 128],
-      sb: [158, 99, 181],
-      te: [212, 122, 0],
-      i: [148, 0, 148],
-      xe: [66, 158, 176],
-      cs: [87, 23, 143],
-      ba: [0, 201, 0],
-      la: [112, 212, 255],
-      ce: [255, 255, 199],
-      pr: [217, 255, 199],
-      nd: [199, 255, 199],
-      pm: [163, 255, 199],
-      sm: [143, 255, 199],
-      eu: [97, 255, 199],
-      gd: [69, 255, 199],
-      tb: [48, 255, 199],
-      dy: [31, 255, 199],
-      ho: [0, 255, 156],
-      er: [0, 230, 117],
-      tm: [0, 212, 82],
-      yb: [0, 191, 56],
-      lu: [0, 171, 36],
-      hf: [77, 194, 255],
-      ta: [77, 166, 255],
-      w: [33, 148, 214],
-      re: [38, 125, 171],
-      os: [38, 102, 150],
-      ir: [23, 84, 135],
-      pt: [208, 208, 224],
-      au: [255, 209, 35],
-      hg: [184, 184, 208],
-      tl: [166, 84, 77],
-      pb: [87, 89, 97],
-      bi: [158, 79, 181],
-      po: [171, 92, 0],
-      at: [117, 79, 69],
-      rn: [66, 130, 150],
-      fr: [66, 0, 102],
-      ra: [0, 125, 0],
-      ac: [112, 171, 250],
-      th: [0, 186, 255],
-      pa: [0, 161, 255],
-      u: [0, 143, 255],
-      np: [0, 128, 255],
-      pu: [0, 107, 255],
-      am: [84, 92, 242],
-      cm: [120, 92, 227],
-      bk: [138, 79, 227],
-      cf: [161, 54, 212],
-      es: [179, 31, 212],
-      fm: [179, 31, 186],
-      md: [179, 13, 166],
-      no: [189, 13, 135],
-      lr: [199, 0, 102],
-      rf: [204, 0, 89],
-      db: [209, 0, 79],
-      sg: [217, 0, 69],
-      bh: [224, 0, 56],
-      hs: [230, 0, 46],
-      mt: [235, 0, 38],
-      ds: [235, 0, 38],
-      rg: [235, 0, 38],
-      cn: [235, 0, 38],
-      uut: [235, 0, 38],
-      uuq: [235, 0, 38],
-      uup: [235, 0, 38],
-      uuh: [235, 0, 38],
-      uus: [235, 0, 38],
-      uuo: [235, 0, 38],
-    };
-
     const atoms = [];
-
     const _bonds = [];
     const _bhash = {};
     const _atomMap = {};
 
     // parse
-
     const lines = text.split('\n');
-
     for (let i = 0, l = lines.length; i < l; i++) {
       if (lines[i].slice(0, 4) === 'ATOM' || lines[i].slice(0, 6) === 'HETATM') {
         const x = parseFloat(lines[i].slice(30, 37));
         const y = parseFloat(lines[i].slice(38, 45));
         const z = parseFloat(lines[i].slice(46, 53));
         const index = parseInt(lines[i].slice(6, 11)) - 1;
-
         let e = trim(lines[i].slice(76, 78)).toLowerCase();
-
         if (e === '') {
           e = trim(lines[i].slice(12, 14)).toLowerCase();
         }
-
-        const atomData = [x, y, z, CPK[e], capitalize(e)];
-
+        const atomData = [x, y, z, e, capitalize(e)];
         atoms.push(atomData);
         _atomMap[index] = atomData;
       } else if (lines[i].slice(0, 6) === 'CONECT') {
         const satom = parseInt(lines[i].slice(6, 11));
-
         parseBond(11, 5, satom, i);
         parseBond(16, 5, satom, i);
         parseBond(21, 5, satom, i);
--- src/molecularViewer.tsx
+++ src/molecularViewer.tsx
@@ -9,7 +9,7 @@ import { Color, Vector3 } from 'three';
 import { Bond } from './Bond';
 import { Molecule } from './Molecule';
 import { Cylinder, Sphere } from '@react-three/drei';
-import { HALF_PI } from './constants';
+import { CPK_COLORS, HALF_PI } from './constants';
 import { useStore } from './stores/common';
 import * as Selector from './stores/selector';
 
@@ -26,29 +26,31 @@ const MolecularViewer = ({ inputMolecule }: MolecularViewerProps) => {
     const pdb = pdbLoader.parse(inputMolecule);
     const geometryAtoms = pdb.geometryAtoms;
     const geometryBonds = pdb.geometryBonds;
+    const elementsBonds: string[] = pdb.elementsBonds;
     const json = pdb.json;
     let positions = geometryAtoms.getAttribute('position');
-    let colors = geometryAtoms.getAttribute('color');
     const atoms: Atom[] = [];
     for (let i = 0; i < positions.count; i++) {
       const atom = json.atoms[i];
+      const c = CPK_COLORS[atom[3]];
       atoms.push({
         name: atom[4] as string,
         position: new Vector3(positions.getX(i), positions.getY(i), positions.getZ(i)),
-        color: new Color(colors.getX(i), colors.getY(i), colors.getZ(i)),
+        color: new Color(c[0] / 255, c[1] / 255, c[2] / 255).convertSRGBToLinear(),
         radius: getChemicalElement(atom[4] as string)?.sigma / 5,
       } as Atom);
     }
     positions = geometryBonds.getAttribute('position');
-    colors = geometryBonds.getAttribute('color');
     const bonds: Bond[] = [];
     for (let i = 0; i < positions.count; i += 2) {
       const j = i + 1;
+      const ci = CPK_COLORS[elementsBonds[i]];
+      const cj = CPK_COLORS[elementsBonds[j]];
       bonds.push({
         start: new Vector3(positions.getX(i), positions.getY(i), positions.getZ(i)),
         end: new Vector3(positions.getX(j), positions.getY(j), positions.getZ(j)),
-        startColor: new Color(colors.getX(i), colors.getY(i), colors.getZ(i)),
-        endColor: new Color(colors.getX(j), colors.getY(j), colors.getZ(j)),
+        startColor: new Color(ci[0] / 255, ci[1] / 255, ci[2] / 255).convertSRGBToLinear(),
+        endColor: new Color(cj[0] / 255, cj[1] / 255, cj[2] / 255).convertSRGBToLinear(),
       } as Bond);
     }
     return { atoms, bonds } as Molecule;
@@ -85,7 +87,8 @@ const MolecularViewer = ({ inputMolecule }: MolecularViewerProps) => {
       <group name={'Bonds'}>
         {molecule.bonds.map((e, index) => {
           const mid = e.start.clone().lerp(e.end, 0.5);
-          const length = e.end.distanceTo(e.start);
+          const fullLength = e.end.distanceTo(e.start);
+          const visibleLength = fullLength;
           return (
             <React.Fragment key={'Bond' + index}>
               <group
@@ -99,7 +102,7 @@ const MolecularViewer = ({ inputMolecule }: MolecularViewerProps) => {
                   name={'Bond1' + index}
                   castShadow={false}
                   receiveShadow={false}
-                  args={[0.1, 0.1, length * 0.5, 16, 1]}
+                  args={[0.1, 0.1, fullLength * 0.5, 16, 1]}
                   rotation={[HALF_PI, 0, 0]}
                 >
                   <meshStandardMaterial attach="material" color={e.startColor} />
@@ -116,7 +119,7 @@ const MolecularViewer = ({ inputMolecule }: MolecularViewerProps) => {
                   name={'Bond2' + index}
                   castShadow={false}
                   receiveShadow={false}
-                  args={[0.1, 0.1, length * 0.5, 16, 1]}
+                  args={[0.1, 0.1, fullLength * 0.5, 16, 1]}
                   rotation={[HALF_PI, 0, 0]}
                 >
                   <meshStandardMaterial attach="material" color={e.endColor} />
