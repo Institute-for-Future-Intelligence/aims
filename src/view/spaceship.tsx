c9971ad965176a7ee77386cca990fa8761a3122a
Charles Xie <charxie@gmail.com>
2024-01-27 20:59:09 -0500

    spaceship

--- src/view/spaceship.tsx
+++ src/view/spaceship.tsx
@@ -7,27 +7,32 @@ import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Euler, Group, Mesh, Vector3 } from 'three';
 import { useGLTF } from '@react-three/drei';
-// @ts-expect-error: What
+// @ts-expect-error: ignore
 import shipUrl from '../assets/ship.gltf';
 
 export interface SpaceshipProps {
   scale?: number;
 }
 
-const Spaceship = React.memo(({ scale = 4 }: SpaceshipProps) => {
+const Spaceship = React.memo(({ scale = 0.2 }: SpaceshipProps) => {
   const { camera } = useThree();
-  const [up, setUp] = useState<Vector3>(new Vector3());
-  const [rotation, setRotation] = useState<Euler>(new Euler());
+  const groupRef = useRef<any>();
 
   useFrame(() => {
-    setUp(camera.up.clone());
-    setRotation(camera.rotation.clone());
+    if (groupRef.current) {
+      const v = new Vector3();
+      camera.getWorldDirection(v);
+      groupRef.current.position.copy(camera.position);
+      groupRef.current.position.addScaledVector(v, 5);
+      groupRef.current.up.copy(camera.up);
+      groupRef.current.rotation.copy(camera.rotation);
+    }
   });
 
   const model = useGLTF(shipUrl);
 
   return (
-    <group rotation={rotation} up={up}>
+    <group ref={groupRef}>
       <mesh position={[0, 0, 0]} rotation={[0, Math.PI, 0]}>
         <primitive object={model.scene} scale={scale} />
       </mesh>
