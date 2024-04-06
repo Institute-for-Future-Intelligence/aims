92ff16945e546aabd8b7da8e1772645dec543d3f
Charles Xie <charxie@gmail.com>
2024-04-05 21:15:21 -0400

    butane force fields

--- src/models/AngularBondDefinition.ts
+++ src/models/AngularBondDefinition.ts
@@ -44,13 +44,13 @@ export const getAngularBondDefinition = (name: string): Triple[] => {
   }
   if (name === 'Propane') {
     return [
-      // C-C-C angle
+      // C-C-C angles
       { i1: 1, i2: 0, i3: 2 },
       // left CH3 group
       { i1: 5, i2: 1, i3: 6 },
       { i1: 6, i2: 1, i3: 7 },
       { i1: 7, i2: 1, i3: 5 },
-      // middle CH2 gropu
+      // middle CH2 group
       { i1: 3, i2: 0, i3: 4 },
       // right CH3 group
       { i1: 8, i2: 2, i3: 9 },
@@ -70,6 +70,45 @@ export const getAngularBondDefinition = (name: string): Triple[] => {
       // H-C-C angles from middle to right
       { i1: 3, i2: 0, i3: 2 },
       { i1: 4, i2: 0, i3: 2 },
+    ];
+  }
+  if (name === 'Butane') {
+    return [
+      // C-C-C angles
+      { i1: 2, i2: 0, i3: 1 },
+      { i1: 0, i2: 1, i3: 3 },
+      // left CH3 group
+      { i1: 8, i2: 2, i3: 9 },
+      { i1: 9, i2: 2, i3: 10 },
+      { i1: 10, i2: 2, i3: 8 },
+      // middle CH2 group
+      { i1: 4, i2: 0, i3: 5 },
+      // middle CH2 group
+      { i1: 7, i2: 1, i3: 6 },
+      // right CH3 group
+      { i1: 11, i2: 3, i3: 12 },
+      { i1: 12, i2: 3, i3: 13 },
+      { i1: 13, i2: 3, i3: 11 },
+      // H-C-C angles from left to middle
+      { i1: 8, i2: 2, i3: 0 },
+      { i1: 9, i2: 2, i3: 0 },
+      { i1: 10, i2: 2, i3: 0 },
+      // C-C-H angles from middle to right
+      { i1: 1, i2: 3, i3: 11 },
+      { i1: 1, i2: 3, i3: 12 },
+      { i1: 1, i2: 3, i3: 13 },
+      // C-C-H angles from left to middle
+      { i1: 2, i2: 0, i3: 4 },
+      { i1: 2, i2: 0, i3: 5 },
+      // H-C-C angles from middle to right
+      { i1: 6, i2: 1, i3: 3 },
+      { i1: 7, i2: 1, i3: 3 },
+      // C-C-H angles from middle to middle
+      { i1: 0, i2: 1, i3: 6 },
+      { i1: 0, i2: 1, i3: 7 },
+      // H-C-C angles from middle to middle
+      { i1: 4, i2: 0, i3: 1 },
+      { i1: 5, i2: 0, i3: 1 },
     ];
   }
   return [];
--- src/models/TorsionalBondDefinition.ts
+++ src/models/TorsionalBondDefinition.ts
@@ -33,5 +33,30 @@ export const getTorsionalBondDefinition = (name: string): Quadruple[] => {
       { i1: 1, i2: 0, i3: 2, i4: 9 },
     ];
   }
+  if (name === 'Butane') {
+    return [
+      // C-C-C-C
+      { i1: 2, i2: 0, i3: 1, i4: 3 },
+      // H-C-C-H from left to middle
+      { i1: 8, i2: 2, i3: 0, i4: 4 },
+      { i1: 8, i2: 2, i3: 0, i4: 5 },
+      { i1: 9, i2: 2, i3: 0, i4: 4 },
+      { i1: 9, i2: 2, i3: 0, i4: 5 },
+      // H-C-C-C from left to right
+      { i1: 8, i2: 2, i3: 0, i4: 1 },
+      { i1: 9, i2: 2, i3: 0, i4: 1 },
+      // H-C-C-H from middle to right
+      { i1: 6, i2: 1, i3: 3, i4: 11 },
+      { i1: 7, i2: 1, i3: 3, i4: 11 },
+      { i1: 6, i2: 1, i3: 3, i4: 12 },
+      { i1: 7, i2: 1, i3: 3, i4: 12 },
+      // C-C-C-H from left to right
+      { i1: 0, i2: 1, i3: 3, i4: 11 },
+      { i1: 0, i2: 1, i3: 3, i4: 12 },
+      // H-C-C-H from middle to middle
+      { i1: 4, i2: 0, i3: 1, i4: 6 },
+      { i1: 5, i2: 0, i3: 1, i4: 7 },
+    ];
+  }
   return [];
 };
